4eacf92a25f4222d72ae078f2d697777d290bb25
Peng <peng@Pengs-MacBook-Pro.local>
2019-06-28 18:13:03 +1000

    Master: update app js wording

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
-app.get('/', (req, res) => res.send('Hello World with test!'))
+app.get('/', (req, res) => res.send('Hello World with test 18:12!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
